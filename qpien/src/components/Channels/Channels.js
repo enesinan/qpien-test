21b5dce3507f7ce3110b79e1d3664d7a880a19f8
enes-dev <inane064@gmail.com>
2021-06-10 14:59:06 +0300

    Channel.js added as a component our react application. İt has three main button

---
+++ qpien/src/components/Channels/Channels.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './channel.css'
+
+function Channels() {
+    return (
+        <div>
+            <div className="channels-title">
+                <h1>Kanallar</h1>
+            </div>
+            <div className="channels-list">
+                     <button className="channels-button-1 space-button style-button">Button</button>
+                     <button className="channels-button-2 space-button style-button">Button</button>
+                     <button className="channels-button-3 space-button style-button">Qpien Chat</button>
+            </div>
+        </div>
+    )
+}
+
+export default Channels
